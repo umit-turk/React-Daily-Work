001446bd205b1d7b1dc4fee19f07c6eb6a7bd93a
Ümit Yaşar TÜRK <76227146+umit-turk@users.noreply.github.com>
2021-04-22 23:40:54 +0300

    Add files via upload

---
+++ context/src/App.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import "./styles.css";
+import UserList from "./UserList";
+import { UserContext } from "./contexts/UserContext";
+
+//prett,er-ignore
+const data = [
+  {
+    id: 1,
+    name: "Ahmet",
+    email: "ahmet@site.com",
+    age: 25,
+    color: "lightcyan"
+  },
+  {
+    id: 2,
+    name: "Mehmet",
+    email: "mehmet@site.com",
+    age: 30,
+    color: "lightblue"
+  },
+  { id: 3, name: "Ali", email: "ali@site.com", age: 35, color: "lightgreen" }
+];
+
+const App = () => {
+  const [users, setUsers] = useState(data);
+
+  const changeColor = (id, color) =>
+    setUsers(
+      users.map((user) => (user.id === id ? { ...user, color: color } : user))
+    );
+
+  return (
+    <UserContext.Provider value={{ users, changeColor }}>
+      <div className="App">
+        <header>
+          <h1>Welcome</h1>
+        </header>
+        <UserList />
+      </div>
+    </UserContext.Provider>
+  );
+};
+
+export default App;
